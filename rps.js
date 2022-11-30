bbf2fa9f0a41f436648bf6d8bac5351f7cb14ac1
Armando <armando.ortiz.jr.19@outlook.com>
2022-11-30 16:16:01 -0600

    Implement logic for determining outcome of a single match

--- rps.js
+++ rps.js
@@ -14,3 +14,37 @@ function getPlayerChoice(){
     }
     return choice
 }
+
+function playPaperRockScissors(computerSelection, playerSelection){
+    console.log(`Computer: ${computerSelection}`);
+    console.log(`Player: ${playerSelection}`);
+    let winnerStatement = `You Win! `;
+    let loserStatement = `You Lose! `;
+    let tieStatement = `You are tied. You both chose ${computerSelection}. Please try again.`;
+    let finalStatement;
+
+    let tieConditional = (computerSelection===playerSelection);
+
+    if (tieConditional){
+        finalStatement = tieStatement;
+    }
+
+    else{
+        let winnerConditional = (playerSelection==="rock" && computerSelection==="scissors") ? true :
+        (playerSelection==="paper" && computerSelection==="rock")? true :
+        (playerSelection==="scissors" && computerSelection==="paper")? true: false;
+
+        if (winnerConditional){
+            winnerStatement += `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`;
+            finalStatement = winnerStatement;
+        }
+
+        else{
+            loserStatement += `${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`;
+            finalStatement = loserStatement;
+        }
+    }
+    return finalStatement;
+}
+
+alert(playPaperRockScissors(getComputerChoice(), getPlayerChoice()));
